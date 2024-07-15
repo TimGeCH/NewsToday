eed378a8236b20d3746448270208b3cfd14ad662
fg128 <fg128@students.waikato.ac.nz>
2024-07-15 19:57:15 +1200

    creat header section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Button } from "flowbite-react";
 
 export default function Dashboard() {
     return (
-        <div>Dashboard</div>
+        <div>Dashboard  </div>
+
     )
 }
